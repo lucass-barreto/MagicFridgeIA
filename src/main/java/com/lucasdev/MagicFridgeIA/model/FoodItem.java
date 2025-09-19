9c5d3455b1a64fda6b86eafded6e5ff9a1685c4b
Lucas <lucasbo.dev@gmail.com>
2025-09-19 18:56:38 -0300

    feat(model): Add FoodItem entity

--- src/main/java/com/lucasdev/MagicFridgeIA/model/FoodItem.java
+++ src/main/java/com/lucasdev/MagicFridgeIA/model/FoodItem.java
@@ -1,4 +1,64 @@
 package com.lucasdev.MagicFridgeIA.model;
 
+import com.lucasdev.MagicFridgeIA.enums.Categoria;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name ="food_item")
+@NoArgsConstructor
+@AllArgsConstructor
 public class FoodItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @Enumerated(EnumType.STRING)
+    private Categoria categoria;
+    private Integer quantidade;
+    private LocalDate validade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public LocalDate getValidade() {
+        return validade;
+    }
+
+    public void setValidade(LocalDate validade) {
+        this.validade = validade;
+    }
 }
