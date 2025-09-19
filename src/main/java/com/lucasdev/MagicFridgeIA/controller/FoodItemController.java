1736a7ab251c14dcb0d0675e5d7e3147b59a235f
Lucas <lucasbo.dev@gmail.com>
2025-09-19 18:57:43 -0300

    feat(controller): Add FoodItemController with REST endpoints

--- src/main/java/com/lucasdev/MagicFridgeIA/controller/FoodItemController.java
+++ src/main/java/com/lucasdev/MagicFridgeIA/controller/FoodItemController.java
@@ -1,4 +1,60 @@
 package com.lucasdev.MagicFridgeIA.controller;
 
+import com.lucasdev.MagicFridgeIA.dto.FoodItemDTO;
+import com.lucasdev.MagicFridgeIA.service.FoodItemService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("/food")
 public class FoodItemController {
+
+    private FoodItemService foodItemService;
+
+    public FoodItemController(FoodItemService foodItemService) {
+        this.foodItemService = foodItemService;
+    }
+
+
+    public ResponseEntity<FoodItemDTO> registrarProduto (@RequestBody FoodItemDTO foodItemDTO){
+        FoodItemDTO novoRegistroDTO = foodItemService.registrarProduto(foodItemDTO);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(novoRegistroDTO);
+    }
+
+    public ResponseEntity<List<FoodItemDTO>> listarRegistros(){
+        List<FoodItemDTO> todosRegistrosDTO = foodItemService.listarRegistros();
+        return ResponseEntity.ok(todosRegistrosDTO);
+    }
+
+    public ResponseEntity<?> listarPorId(@PathVariable Long id){
+        FoodItemDTO itemEncontradoDTO = foodItemService.listarPorId(id);
+
+        if(itemEncontradoDTO == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .build();
+        } return ResponseEntity.ok(itemEncontradoDTO);
+    }
+
+    public ResponseEntity<?> deletarPorId (@PathVariable Long id){
+        if (foodItemService.deletarPorId(id)){
+            return ResponseEntity.noContent()
+                    .build();
+        } return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .build();
+    }
+
+    public ResponseEntity<FoodItemDTO> alterarItem(@PathVariable Long id, @RequestBody FoodItemDTO foodItemAtualizadoDTO){
+        FoodItemDTO itemAlteradoDTO = foodItemService.alterarItem(id,foodItemAtualizadoDTO);
+
+        if (itemAlteradoDTO == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .build();
+        } return ResponseEntity.ok(itemAlteradoDTO);
+    }
 }
