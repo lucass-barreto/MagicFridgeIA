0f649f87c73194b6e7187da32a7dd13e8b08a94d
Lucas <lucasbo.dev@gmail.com>
2025-09-19 18:56:48 -0300

    feat(model): Add Category enum

--- src/main/java/com/lucasdev/MagicFridgeIA/enums/Categoria.java
+++ src/main/java/com/lucasdev/MagicFridgeIA/enums/Categoria.java
@@ -1,4 +1,12 @@
 package com.lucasdev.MagicFridgeIA.enums;
 
 public enum Categoria {
+    CARNE,
+    AVES,
+    FRUTO_DO_MAR,
+    VEGETAIS,
+    FRUTAS,
+    LATICINIOS,
+    OVOS,
+    OUTROS
 }
