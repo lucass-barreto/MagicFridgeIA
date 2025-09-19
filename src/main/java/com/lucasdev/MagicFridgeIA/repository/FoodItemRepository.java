0749964c6c50784892e458d768a5f1a6363684d7
Lucas <lucasbo.dev@gmail.com>
2025-09-19 18:56:58 -0300

    feat(repository): Add FoodItemRepository interface

--- src/main/java/com/lucasdev/MagicFridgeIA/repository/FoodItemRepository.java
+++ src/main/java/com/lucasdev/MagicFridgeIA/repository/FoodItemRepository.java
@@ -2,6 +2,8 @@ package com.lucasdev.MagicFridgeIA.repository;
 
 import com.lucasdev.MagicFridgeIA.model.FoodItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FoodItemRepository extends JpaRepository <FoodItem, Long> {
 }
