2198d6d20d7bf6566ceca6e9d461146dcea8be68
Lucas <lucasbo.dev@gmail.com>
2025-09-20 21:18:10 -0300

    feat(config): Configure WebClient bean for OpenAI API

---
+++ src/main/java/com/lucasdev/MagicFridgeIA/config/WebClientConfig.java
@@ -0,0 +1,18 @@
+package com.lucasdev.MagicFridgeIA.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Value("${https://api.openai.com/v1/responses}")
+    private String chatGptApiUrl;
+
+    @Bean
+    public WebClient webClient (WebClient.Builder builder){
+        return builder.baseUrl(chatGptApiUrl).build();
+    }
+}
