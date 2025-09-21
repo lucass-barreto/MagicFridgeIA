070dc0ebb51a8ef8f3226fe740cd818620536789
Lucas <lucasbo.dev@gmail.com>
2025-09-20 21:16:20 -0300

    fix(dto): Add Lombok's @Data annotation to FoodItemDTO

--- src/main/java/com/lucasdev/MagicFridgeIA/dto/FoodItemDTO.java
+++ src/main/java/com/lucasdev/MagicFridgeIA/dto/FoodItemDTO.java
@@ -1,8 +1,15 @@
 package com.lucasdev.MagicFridgeIA.dto;
 
 import com.lucasdev.MagicFridgeIA.enums.Categoria;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FoodItemDTO {
 
     private Long id;
